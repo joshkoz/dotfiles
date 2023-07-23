a782336a01af788ca57ccb3b1093cdc138ea8106
Josh Koz <josh@joshkoz.com>
2023-07-23 20:30:53 +1000

    use onedark color scheme

--- neovim/.config/nvim/lua/user/plugins/community.lua
+++ neovim/.config/nvim/lua/user/plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim", enabled = true },
+  {
+    "olimorris/onedarkpro.nvim",
+    event = "VeryLazy",
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
