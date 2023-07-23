c6a3a2c22445117fc3b22dff7f0856e86622ffac
Josh Koz <josh@joshkoz.com>
2023-07-23 21:38:59 +1000

    add exa ls aliases

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,1 +1,13 @@
 alias k=kubectl
+
+
+# Use Exa Aliases if exa is installed 
+if command -v exa &> /dev/null; then
+  alias ls=exa
+  alias ls='exa'                                                          # ls
+  alias l='exa -lbF'
+  alias ll='exa -lbGF'
+  alias llm='exa -lbGd --sort=modified'
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --color-scale'
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --color-scale'
+fi
