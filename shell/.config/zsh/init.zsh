5e6961d204f8b9d379ea5281a15cf5b2f0e35dd4
Josh Koz <josh@joshkoz.com>
2023-08-04 15:36:47 +1000

    add zsh init config

--- shell/.config/zsh/init.zsh
+++ shell/.config/zsh/init.zsh
@@ -1,5 +1,14 @@
 eval "$(starship init zsh)"
 
+export HISTFILE=~/.histfile
+export HISTSIZE=1000
+export SAVEHIST=1000
+unsetopt beep
+bindkey -v
+zstyle :compinstall filename '/home/joshua/.zshrc'
+
+autoload -Uz compinit
+compinit
 # Only use the 1Password agent bridge if we're in WSL
 if grep -iq microsoft /proc/version; then
   source $HOME/.config/zsh/scripts/.agent-bridge.sh
