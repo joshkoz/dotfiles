902108ed8f7e7c5bb53c0dd215a365e7727cc2e5
Josh Koz <josh@joshkoz.com>
2023-08-22 23:54:31 +1000

    tweak omnisharp lsp config in neovim

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,19 @@ local servers = {
   rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html' } },
-  omnisharp = {},
+  omnisharp = {
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#omnisharp
+    -- If true, MSBuild project system will only load projects for files that
+    -- were opened in the editor. This setting is useful for big C# codebases
+    -- and allows for faster initialization of code navigation features only
+    -- for projects that are relevant to code that is being edited. With this
+    -- setting enabled OmniSharp may load fewer projects and may thus display
+    -- incomplete reference lists for symbols.
+    enable_ms_build_load_projects_on_demand = false,
+    enable_editorconfig_support = true,
+    enable_import_completion = true,
+    enable_roslyn_analyzers = true
+  },
   lua_ls = {
     settings = {
       Lua = {
@@ -26,6 +38,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       { 'folke/neodev.nvim', opts = {} },
+      "Hoffs/omnisharp-extended-lsp.nvim",
       "mason.nvim",
       'williamboman/mason-lspconfig.nvim',
       { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
@@ -38,6 +51,7 @@ return {
           spacing = 4,
           source = "if_many",
           prefix = "●",
+          severity = { min = vim.diagnostic.severity.WARN }
           -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
           -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
           -- prefix = "icons",
@@ -46,10 +60,8 @@ return {
       },
 
       inlay_hints = {
-        enabled = false,
+        enabled = true,
       },
-      -- add any global capabilities here
-      capabilities = {},
       -- Automatically format on save
       autoformat = true,
       -- Enable this to show formatters used in a notification
@@ -68,18 +80,10 @@ return {
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
+      setup = {},
     },
     config = function()
-      local lspconfig = require("mason-lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
       capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -113,89 +117,101 @@ return {
         end, { desc = 'Format current buffer with LSP' })
       end
 
-      lspconfig.setup {
+      mason_lspconfig.setup {
         ensure_installed = vim.tbl_keys(servers),
       }
 
-      lspconfig.setup_handlers {
+      mason_lspconfig.setup_handlers {
         function(server_name)
-          require('lspconfig')[server_name].setup {
+          local config = {
             capabilities = capabilities,
             on_attach = on_attach,
             settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
+            filetypes = (servers[server_name] or {}).filetypes
           }
+
+          if server_name == "omnisharp" then
+            config.handlers = {
+              -- https://github.com/Hoffs/omnisharp-extended-lsp.nvim
+              ["textDocument/definition"] = require('omnisharp_extended').handler,
+              -- Make warning the minimum level shown for csharp
+              ["textDocument/publishDiagnostics"] = vim.lsp.with(
+                vim.lsp.diagnostic.on_publish_diagnostics, {
+                  virtual_text = {
+                    severity_limit = 'Warning',
+                  },
+                })
+            }
+          end
+
+          require('lspconfig')[server_name].setup(config)
         end
       }
-      -- --https://github.com/aca/emmet-ls
-      -- lspconfig.emmet_ls.setup({
-      --   filetypes = { "html", "css", "template", "javascript" },
-      -- })
 
-      -- Switch for controlling whether you want autoformatting.
-      --  Use :KickstartFormatToggle to toggle autoformatting on or off
-      local format_is_enabled = true
-      vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-        format_is_enabled = not format_is_enabled
-        print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-      end, {})
-
-      -- Create an augroup that is used for managing our formatting autocmds.
-      --      We need one augroup per client to make sure that multiple clients
-      --      can attach to the same buffer without interfering with each other.
-      local _augroups = {}
-      local get_augroup = function(client)
-        if not _augroups[client.id] then
-          local group_name = 'kickstart-lsp-format-' .. client.name
-          local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-          _augroups[client.id] = id
-        end
-
-        return _augroups[client.id]
-      end
-
-      -- Whenever an LSP attaches to a buffer, we will run this function.
+      -- -- Switch for controlling whether you want autoformatting.
+      -- --  Use :KickstartFormatToggle to toggle autoformatting on or off
+      -- local format_is_enabled = true
+      -- vim.api.nvim_create_user_command('KickstartFormatToggle', function()
+      --   format_is_enabled = not format_is_enabled
+      --   print('Setting autoformatting to: ' .. tostring(format_is_enabled))
+      -- end, {})
       --
-      -- See `:help LspAttach` for more information about this autocmd event.
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-        -- This is where we attach the autoformatting for reasonable clients
-        callback = function(args)
-          local client_id = args.data.client_id
-          local client = vim.lsp.get_client_by_id(client_id)
-          local bufnr = args.buf
-
-          -- Only attach to clients that support document formatting
-          if not client.server_capabilities.documentFormattingProvider then
-            return
-          end
-
-          -- Tsserver usually works poorly. Sorry you work with bad languages
-          -- You can remove this line if you know what you're doing :)
-          if client.name == 'tsserver' then
-            return
-          end
-
-          -- Create an autocmd that will run *before* we save the buffer.
-          --  Run the formatting command for the LSP that has just attached.
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = get_augroup(client),
-            buffer = bufnr,
-            callback = function()
-              if not format_is_enabled then
-                return
-              end
-
-              vim.lsp.buf.format {
-                async = false,
-                filter = function(c)
-                  return c.id == client.id
-                end,
-              }
-            end,
-          })
-        end,
-      })
+      -- -- Create an augroup that is used for managing our formatting autocmds.
+      -- --      We need one augroup per client to make sure that multiple clients
+      -- --      can attach to the same buffer without interfering with each other.
+      -- local _augroups = {}
+      -- local get_augroup = function(client)
+      --   if not _augroups[client.id] then
+      --     local group_name = 'kickstart-lsp-format-' .. client.name
+      --     local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+      --     _augroups[client.id] = id
+      --   end
+      --
+      --   return _augroups[client.id]
+      -- end
+      --
+      -- -- Whenever an LSP attaches to a buffer, we will run this function.
+      -- --
+      -- -- See `:help LspAttach` for more information about this autocmd event.
+      -- vim.api.nvim_create_autocmd('LspAttach', {
+      --   group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+      --   -- This is where we attach the autoformatting for reasonable clients
+      --   callback = function(args)
+      --     local client_id = args.data.client_id
+      --     local client = vim.lsp.get_client_by_id(client_id)
+      --     local bufnr = args.buf
+      --
+      --     -- Only attach to clients that support document formatting
+      --     if not client.server_capabilities.documentFormattingProvider then
+      --       return
+      --     end
+      --
+      --     -- Tsserver usually works poorly. Sorry you work with bad languages
+      --     -- You can remove this line if you know what you're doing :)
+      --     if client.name == 'tsserver' then
+      --       return
+      --     end
+      --
+      --     -- Create an autocmd that will run *before* we save the buffer.
+      --     --  Run the formatting command for the LSP that has just attached.
+      --     vim.api.nvim_create_autocmd('BufWritePre', {
+      --       group = get_augroup(client),
+      --       buffer = bufnr,
+      --       callback = function()
+      --         if not format_is_enabled then
+      --           return
+      --         end
+      --
+      --         vim.lsp.buf.format {
+      --           async = false,
+      --           filter = function(c)
+      --             return c.id == client.id
+      --           end,
+      --         }
+      --       end,
+      --     })
+      --   end,
+      -- })
     end,
   },
   { 'williamboman/mason-lspconfig.nvim', opts = {} }
