a4a941566310f6345caf199e7a778f6064c3e087
Josh Koz <josh@joshkoz.com>
2023-08-27 00:47:41 +1000

    add cs snippet to populate class file with namespace

--- neovim/.config/nvim/lua/plugins/autocomplete.lua
+++ neovim/.config/nvim/lua/plugins/autocomplete.lua
@@ -18,6 +18,7 @@ return
     local luasnip = require 'luasnip'
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
+    require('config.snippets');
 
     cmp.setup {
       snippet = {
