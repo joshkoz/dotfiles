6e49c2e5398ba473d9ab3bcaedced53549029a15
Josh Koz <josh@joshkoz.com>
2023-09-04 21:42:20 +1000

    add --hiden to telescope vimgrep_arguments

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -14,6 +14,16 @@ return {
         file_ignore_patterns = {
           "^.git/",
         },
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+        },
         mappings = {
           n = { q = actions.close },
           i = {
