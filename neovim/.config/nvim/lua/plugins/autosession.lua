b88b0f708c8efa1d1acde2af5c57855de2efbc88
Josh Koz <josh@joshkoz.com>
2023-09-04 17:34:46 +1000

    add autosession plugin to neovim

---
+++ neovim/.config/nvim/lua/plugins/autosession.lua
@@ -0,0 +1,16 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      pre_save_cmds = { 'Neotree close' },
+      pre_restore_cmds = { 'Neotree toggle' }
+    })
+
+    vim.keymap.set("n", "<leader>sr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })             -- restore last workspace session for current directory
+    vim.keymap.set("n", "<leader>sw", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+  end,
+}
