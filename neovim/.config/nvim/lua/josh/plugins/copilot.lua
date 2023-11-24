17367eb790596149711d62fdb83c8ebedea55c3b
Josh Koz <josh@joshkoz.com>
2023-11-24 22:15:25 +1100

    Add copilot and gp to neovim

---
+++ neovim/.config/nvim/lua/josh/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'github/copilot.vim',
+    lazy = false,
+    config = function() end,
+  },
+  -- {
+  --   'zbirenbaum/copilot-cmp',
+  --   dependencies = { 'github/copilot.vim' },
+  --   config = function()
+  --     require('copilot_cmp').setup()
+  --   end,
+  -- },
+}
