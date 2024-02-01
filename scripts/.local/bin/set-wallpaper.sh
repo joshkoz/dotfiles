a499a89e6c6a9e9f89c235f0c2064c5ccf938427
Josh Koz <josh@joshkoz.com>
2024-02-01 22:24:59 +1100

    add script to set wallpaper

---
+++ scripts/.local/bin/set-wallpaper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# nasa-wallpaper -q earth -d $(date -d "yesterday" '+%Y-%m-%d') 
+
+# Directory containing wallpapers
+wallpaper_directory="/home/joshua/Pictures/wallpapers/"
+
+# Select a random file from the directory
+# uncooment the below to randomise 
+#selected_file=$(find "$wallpaper_directory" -type f | shuf -n 1)
+selected_file=/home/joshua/Pictures/wallpapers/karman-line-wide.jpg
+
+# Set the selected file as the wallpaper
+# Replace 'feh' with your wallpaper setting command if different
+feh --bg-scale --no-xinerama "$selected_file"
+
+# Extract filename from the path
+filename=$(basename "$selected_file")
+
+# Send notification with Dunst
+# Note: Dunst may have limitations on image sizes and types it can display
+dunstify -i "$selected_file" "Wallpaper Changed" "Wallpaper set to $filename"
+
+
