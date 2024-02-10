8ce2b5851f5ddfccc06f7a472e837dd4a4973296
Josh Koz <josh@joshkoz.com>
2024-02-10 19:15:26 +1100

    update wallpaper script

--- scripts/.local/bin/set-wallpaper.sh
+++ scripts/.local/bin/set-wallpaper.sh
@@ -8,8 +8,13 @@ wallpaper_directory="/home/joshua/Pictures/wallpapers/"
 
 # Select a random file from the directory
 # uncooment the below to randomise 
-#selected_file=$(find "$wallpaper_directory" -type f | shuf -n 1)
-selected_file=/home/joshua/Pictures/wallpapers/karman-line-wide.jpg
+if [ -z "$1" ]; then
+  selected_file=$(find "$wallpaper_directory" -type f | shuf -n 1)
+  dunstify -i "$selected_file" "Wallpaper Changed" "Wallpaper set to $filename"
+else
+  selected_file=$1
+fi
+# selected_file=/home/joshua/Pictures/wallpapers/karman-line-wide.jpg
 
 # Set the selected file as the wallpaper
 # Replace 'feh' with your wallpaper setting command if different
@@ -20,6 +25,5 @@ filename=$(basename "$selected_file")
 
 # Send notification with Dunst
 # Note: Dunst may have limitations on image sizes and types it can display
-dunstify -i "$selected_file" "Wallpaper Changed" "Wallpaper set to $filename"
 
 
