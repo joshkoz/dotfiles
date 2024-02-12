23d2f7bf3f469b33336e738bc5178a467d0719c3
Josh Koz <josh@joshkoz.com>
2024-02-12 23:03:16 +1100

    update colors script

--- scripts/.local/bin/colors.sh
+++ scripts/.local/bin/colors.sh
@@ -17,7 +17,7 @@ colors=(
 "kanagawa_gray #363646"
 "kanagawa_magenta #957fb8"
 "kanagawa_pink #938aa9"
-"kanagawa_red #"
+"kanagawa_red #c34043"
 "kanagawa_green #76946a"
 "kanagawa_yellow #c0a36e"
 "kanagawa_blue #7e9cd8"
@@ -28,8 +28,15 @@ colors=(
 # Convert array to a newline-separated string
 color_list=$(printf "%s\n" "${colors[@]}")
 
-# Use fzf with a preview window at the bottom
-selected_color=$(echo "$color_list" | fzf --preview 'echo {} | awk "{split(\$NF,a,\"#\"); r=strtonum(\"0x\" substr(a[2],1,2)); g=strtonum(\"0x\" substr(a[2],3,2)); b=strtonum(\"0x\" substr(a[2],5,2)); printf \"\033[48;2;%d;%d;%dm%20s\033[0m\n\", r, g, b, \"\" }"' --preview-window "bottom:50%:wrap")
+if [ "$1" = "shuf-list" ]; then
+  selected_color=$(shuf -n 1 <<< "$color_list")
+elif [ "$1" = "random" ]; then
+  selected_color='random'
+elif [ -n "$1" ]; then
+  selected_color="$1"
+else
+  selected_color=$(echo "$color_list" | fzf --preview 'echo {} | awk "{split(\$NF,a,\"#\"); r=strtonum(\"0x\" substr(a[2],1,2)); g=strtonum(\"0x\" substr(a[2],3,2)); b=strtonum(\"0x\" substr(a[2],5,2)); printf \"\033[48;2;%d;%d;%dm%20s\033[0m\n\", r, g, b, \"\" }"' --preview-window "bottom:50%:wrap")
+fi
 
 # Extract the hex code from the selection
 hex_code=$(echo "$selected_color" | awk '{print $NF}')
@@ -45,7 +52,8 @@ graphicscard=1
 desklamp=3
 
 openrgb \
+    --noautoconnect \
     -d $desklamp -c $hex \
     -d $motherboard -c $blue \
-    -d $graphicscard -c $blue
+    -d $graphicscard -c $blue &
 
