3dc3c1ff34e28c4433e8b807f4bc8c24c6b7b6ac
Josh Koz <josh@joshkoz.com>
2024-03-16 12:08:28 +1100

    update kepmaps to keep more defaults

--- neovim/.config/nvim/lua/josh/core/keymaps.lua
+++ neovim/.config/nvim/lua/josh/core/keymaps.lua
@@ -2,18 +2,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Keymap for better default experience
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true, desc = "Unmap space from moving right in normal mode" })
+vim.keymap.set({ "n", "v" }, "<Space>e", vim.cmd.Ex, { silent = true, desc = "Open the file explorer" })
 
--- Open the file explorer
-vim.keymap.set({ "n", "v" }, "<Space>e", vim.cmd.Ex, { silent = true })
-
--- Go to alternate buffer
 vim.keymap.set("n", "<leader><leader>", "<cmd>b#<cr>", { desc = "Go to the alternate buffer" })
 
+vim.keymap.set("x", "<leader>p", [["_P]], { desc = "Paste without yank" })
+vim.keymap.set("n", "<leader>d", [["_d]], { desc = "Delete without yank" })
+
 -- Clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank visual selection into clipboard" })
-vim.keymap.set("n", "d", "\"_d", { desc = "Delete to black-hold register" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank into clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line into clipboard" })
 
 -- Moving a visual selection up or down a line with J and K
 vim.keymap.set("v", "K", ":m '<-2<CR>gvgv=gv", { desc = "Move visual selection up a line" }) -- weird timing bug with only gv=gv for more than 3 lines selected
@@ -28,12 +27,10 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 
 -- Diffs
-vim.keymap.set("n", "<leader>gh", "<cmd>diffget  //2<CR>")
-vim.keymap.set("n", "<leader>gl", "<cmd>diffget  //3<CR>")
+vim.keymap.set("n", "<leader>dl", "<cmd>diffget //2<CR>", { desc = "Diffget from the left" })
+vim.keymap.set("n", "<leader>dr", "<cmd>diffget //3<CR>", { desc = "Diffget from the right" })
 
 --- Quickfix
-vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "Go to next item in Quickfix List" })
-vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Go to previous item in Quickfix List" })
 vim.keymap.set("n", "<A-j>", "<cmd>cnext<CR>zz", { desc = "Go to next item in Quickfix List" })
 vim.keymap.set("n", "<A-k>", "<cmd>cprev<CR>zz", { desc = "Go to previous item in Quickfix List" })
 vim.keymap.set("n", "<leader>co", "<cmd>copen<cr>", { desc = "Open the Quickfix list" })
