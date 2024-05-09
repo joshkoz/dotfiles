4e5fe17e9384a26a06d49fababceb89610580007
Josh Koz <josh@joshkoz.com>
2024-05-09 19:55:44 +1000

    remove exa bindings

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,17 +1,6 @@
 alias k=kubectl
 alias vim=nvim
 
-# Use eza Aliases if eza is installed 
-if command -v eza &> /dev/null; then
-  alias ls=eza
-  alias ls='eza'                                                          # ls
-  alias l='eza -lbF'
-  alias ll='eza -lbGF'
-  alias llm='eza -lbGd --sort=modified'
-  alias la='eza -lbhHigUmuSa --time-style=long-iso --color-scale'
-  alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --color-scale'
-fi
-
 bindkey -s '^f' "^utmux-sessionizer^M"
 
 alias standby="i3lock --color=000000 && systemctl suspend"
