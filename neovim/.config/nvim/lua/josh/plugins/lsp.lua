d2ec9336cbc60f4a60dcb129051870bd15dfc5b5
Josh Koz <josh@joshkoz.com>
2024-05-30 22:06:46 +1000

    update neovim plugins

--- neovim/.config/nvim/lua/josh/plugins/lsp.lua
+++ neovim/.config/nvim/lua/josh/plugins/lsp.lua
@@ -40,12 +40,19 @@ return {
       },
     },
     config = function()
-      -- Change the Diagnostic symbols in the sign column (gutter)
-      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = "󰠠 ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+          },
+        },
+      })
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("LspKeymaps", {}),
