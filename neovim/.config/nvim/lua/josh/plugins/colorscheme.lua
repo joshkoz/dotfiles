76bdf4e2e0f087e5ecda69e685c2c0403f2ecf20
Josh Koz <josh@joshkoz.com>
2024-07-23 23:33:20 +1000

    update colorscheme

--- neovim/.config/nvim/lua/josh/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/josh/plugins/colorscheme.lua
@@ -24,20 +24,8 @@ return {
     name = "catppuccin",
     config = function()
       require("catppuccin").setup({
-        color_overrides = {
-          mocha = {
-            base = "#0d1117",
-            mantle = "#0d1117",
-            crust = "#0d1117",
-          },
-        },
-        highlight_overrides = {
-          mocha = function(colors)
-            return {
-              WinSeparator = { fg = "#363646" },
-            }
-          end,
-        },
+        term_colors = true,
+        transparent_background = true,
       })
       vim.cmd.colorscheme("catppuccin-mocha")
     end,
