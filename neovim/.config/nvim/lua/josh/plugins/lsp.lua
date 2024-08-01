a67988fcee61f74676080b18a61848234176675d
Josh Koz <josh@joshkoz.com>
2024-08-01 21:40:28 +1000

    neovim: update plugins

--- neovim/.config/nvim/lua/josh/plugins/lsp.lua
+++ neovim/.config/nvim/lua/josh/plugins/lsp.lua
@@ -188,32 +188,29 @@ return {
       })
 
       require("roslyn").setup({
-        dotnet_cmd = "dotnet", -- this is the default
-        roslyn_version = "4.8.0-3.23475.7", -- this is the default
-        capabilities = vim.tbl_deep_extend("force", capabilities or {}, {
-          textDocument = {
-            diagnostic = {
-              dynamicRegistration = true,
+        exe = {
+          "dotnet",
+          vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+        },
+        config = {
+          on_attach = function()
+            vim.cmd([[compiler dotnet]])
+          end,
+          settings = {
+            ["csharp|inlay_hints"] = {
+              ["csharp_enable_inlay_hints_for_implicit_object_creation"] = true,
+              ["csharp_enable_inlay_hints_for_implicit_variable_types"] = true,
+              ["csharp_enable_inlay_hints_for_lambda_parameter_types"] = true,
+              ["csharp_enable_inlay_hints_for_types"] = true,
+              ["dotnet_enable_inlay_hints_for_indexer_parameters"] = true,
+              ["dotnet_enable_inlay_hints_for_literal_parameters"] = true,
+              ["dotnet_enable_inlay_hints_for_object_creation_parameters"] = true,
+              ["dotnet_enable_inlay_hints_for_other_parameters"] = true,
+              ["dotnet_enable_inlay_hints_for_parameters"] = true,
+              ["dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix"] = true,
+              ["dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name"] = true,
+              ["dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent"] = true,
             },
-          },
-        }),
-        on_attach = function()
-          vim.cmd([[compiler dotnet]])
-        end,
-        settings = {
-          ["csharp|inlay_hints"] = {
-            ["csharp_enable_inlay_hints_for_implicit_object_creation"] = true,
-            ["csharp_enable_inlay_hints_for_implicit_variable_types"] = true,
-            ["csharp_enable_inlay_hints_for_lambda_parameter_types"] = true,
-            ["csharp_enable_inlay_hints_for_types"] = true,
-            ["dotnet_enable_inlay_hints_for_indexer_parameters"] = true,
-            ["dotnet_enable_inlay_hints_for_literal_parameters"] = true,
-            ["dotnet_enable_inlay_hints_for_object_creation_parameters"] = true,
-            ["dotnet_enable_inlay_hints_for_other_parameters"] = true,
-            ["dotnet_enable_inlay_hints_for_parameters"] = true,
-            ["dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix"] = true,
-            ["dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name"] = true,
-            ["dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent"] = true,
           },
         },
       })
