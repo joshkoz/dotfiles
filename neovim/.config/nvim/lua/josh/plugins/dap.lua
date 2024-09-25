ae0e4e96dd7ebfa0cef4cb8a1ad82be3821b3afd
Josh Koz <josh@joshkoz.com>
2024-09-25 23:13:07 +1000

    neovim: disable nvim-dap

--- neovim/.config/nvim/lua/josh/plugins/dap.lua
+++ neovim/.config/nvim/lua/josh/plugins/dap.lua
@@ -1,52 +1,51 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      { "rcarriga/nvim-dap-ui", opts = {} },
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      vim.keymap.set("n", "<F5>", dap.continue)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<F12>", dap.step_out)
-      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-      vim.keymap.set("n", "<Leader>dB", dap.set_breakpoint, { desc = "Set Breakpoint" })
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      -- Adapters:
-
-      dap.adapters.coreclr = {
-        type = "executable",
-        command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
-        args = { "--interpreter=vscode" },
-      }
-
-      dap.configurations.cs = {
-        {
-          type = "coreclr",
-          name = "launch - netcoredbg",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-          end,
-        },
-      }
-      vim.cmd("highlight DapRed guifg=#E82424")
-      vim.fn.sign_define("DapStopped", { text = "➡️", texthl = "Warning", linehl = "Visual", numhl = "" })
-      vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapRed", linehl = "", numhl = "" })
-    end,
+  "mfussenegger/nvim-dap",
+  enabled = false,
+  dependencies = {
+    { "rcarriga/nvim-dap-ui", opts = {} },
+    "nvim-neotest/nvim-nio",
   },
+  config = function()
+    local dap, dapui = require("dap"), require("dapui")
+    vim.keymap.set("n", "<F5>", dap.continue)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<F12>", dap.step_out)
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+    vim.keymap.set("n", "<Leader>dB", dap.set_breakpoint, { desc = "Set Breakpoint" })
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- Adapters:
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+        end,
+      },
+    }
+    vim.cmd("highlight DapRed guifg=#E82424")
+    vim.fn.sign_define("DapStopped", { text = "➡️", texthl = "Warning", linehl = "Visual", numhl = "" })
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapRed", linehl = "", numhl = "" })
+  end,
 }
