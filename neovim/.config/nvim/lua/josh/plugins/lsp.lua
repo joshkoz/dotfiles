756c01479dde6909df645ed7402fa6eff5fe681a
Josh Koz <josh@joshkoz.com>
2024-12-10 22:17:21 +1100

    neovim: update plugins

--- neovim/.config/nvim/lua/josh/plugins/lsp.lua
+++ neovim/.config/nvim/lua/josh/plugins/lsp.lua
@@ -216,15 +216,31 @@ return {
   {
     "seblj/roslyn.nvim",
     opts = {
-      exe = {
-        "dotnet",
-        vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
-      },
+      -- exe = {
+      --   "dotnet",
+      --   print(vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"))
+      -- },
+      -- args = {
+      --   "--logLevel=Information",
+      --   "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+      -- },
       config = {
         on_attach = function()
           vim.cmd([[compiler dotnet]])
         end,
         settings = {
+          ["csharp|symbol_search"] = {
+            ["dotnet_search_reference_assemblies"] = true,
+          },
+          ["csharp|completion"] = {
+            ["dotnet_enable_references_code_lens"] = true,
+            ["dotnet_show_completion_items_from_unimported_namespaces"] = true,
+            ["dotnet_show_name_completion_suggestions"] = true,
+          },
+          ["csharp|code_lens"] = {
+            ["dotnet_enable_references_code_lens"] = true,
+            ["dotnet_enable_tests_code_lens"] = true,
+          },
           ["csharp|inlay_hints"] = {
             ["csharp_enable_inlay_hints_for_implicit_object_creation"] = true,
             ["csharp_enable_inlay_hints_for_implicit_variable_types"] = true,
