c5b3d3cf2b1a671a3b581bffc1417b34f54ff644
Josh Koz <josh@joshkoz.com>
2024-12-23 18:58:10 +1100

    neovim: update plugins

--- neovim/.config/nvim/lua/josh/plugins/autocomplete.lua
+++ neovim/.config/nvim/lua/josh/plugins/autocomplete.lua
@@ -2,7 +2,7 @@ return {
   {
     "saghen/blink.cmp",
     dependencies = { "rafamadriz/friendly-snippets" },
-    version = "v0.*",
+    version = "*",
     opts = {
       keymap = { preset = "default", ["<CR>"] = { "accept", "fallback" } },
       appearance = {
@@ -14,12 +14,19 @@ return {
           selection = "manual",
         },
       },
-      providers = {
-        lsp = { fallback_for = { "lazydev" } },
-        lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
-      },
       signature = { enabled = true },
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
     },
-    opts_extend = { "sources.default", "lazydev" },
+    opts_extend = { "sources.default" },
   },
 }
