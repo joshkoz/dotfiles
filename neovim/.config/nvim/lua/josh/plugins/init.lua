becf76f2ec6a8e30471c34ca97cf09000dc2082a
Josh Koz <josh@joshkoz.com>
2024-12-27 19:24:55 +1100

    neovim: fix highlights of notify floating window

--- neovim/.config/nvim/lua/josh/plugins/init.lua
+++ neovim/.config/nvim/lua/josh/plugins/init.lua
@@ -61,51 +61,53 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    ---@type snacks.Config
-    opts = {
-      bigfile = { enabled = true },
-      dashboard = { enabled = false },
-      indent = {
-        enabled = true,
-        animate = { enabled = false },
-        chunk = { enabled = false },
-        scope = { enabled = true, hl = "LineNr", char = "│" },
-        -- scope = { enabled = true, hl = "Comment", char = "│" },
+    opts = function()
+      vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+      return {
+        bigfile = { enabled = true },
+        dashboard = { enabled = false },
         indent = {
-          char = "┆", -- | ¦ ┆ ┊ │
-          hl = "LineNr",
-        },
-      },
-      scope = { enabled = false },
-      input = {
-        enabled = true,
-        icon = "",
-        win = {
-          wo = {
-            winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:FloatBorder,SnacksInputBorder:FloatBorder",
+          enabled = true,
+          animate = { enabled = false },
+          chunk = { enabled = false },
+          scope = { enabled = true, hl = "LineNr", char = "│" },
+          -- scope = { enabled = true, hl = "Comment", char = "│" },
+          indent = {
+            char = "┆", -- | ¦ ┆ ┊ │
+            hl = "LineNr",
           },
-          border = "rounded",
+        },
+        scope = { enabled = false },
+        input = {
+          enabled = true,
+          icon = "",
+          win = {
+            wo = {
+              winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:FloatBorder,SnacksInputBorder:FloatBorder",
+            },
+            border = "rounded",
+            style = "minimal",
+            title_pos = "left",
+            relative = "cursor",
+            row = -3,
+            col = 0,
+            keys = {
+              v_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n" },
+              i_esc = { "<esc>", "stopinsert", mode = "i" },
+            },
+          },
+        },
+        notifier = {
+          enabled = true,
+          top_down = false,
           style = "minimal",
-          title_pos = "left",
-          relative = "cursor",
-          row = -3,
-          col = 0,
-          keys = {
-            v_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n" },
-            i_esc = { "<esc>", "stopinsert", mode = "i" },
-          },
+          margin = { bottom = 2 },
         },
-      },
-      notifier = {
-        enabled = true,
-        top_down = false,
-        style = "minimal",
-        margin = { bottom = 2 },
-      },
-      quickfile = { enabled = false },
-      scroll = { enabled = false },
-      statuscolumn = { enabled = true, left = { "mark", "sign" }, right = { "fold", "git" } },
-      words = { enabled = false },
-    },
+        quickfile = { enabled = false },
+        scroll = { enabled = false },
+        statuscolumn = { enabled = true, left = { "mark", "sign" }, right = { "fold", "git" } },
+        words = { enabled = false },
+      }
+    end,
   },
 }
