68c7b50397c647a7825c460d4a9ebb911e883eca
Josh Koz <josh@joshkoz.com>
2025-01-02 21:09:32 +1100

    feat: add yazi

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -4,3 +4,12 @@ alias vim=nvim
 bindkey -s '^f' "^utmux-sessionizer^M"
 
 alias standby="i3lock --color=000000 && systemctl suspend"
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
