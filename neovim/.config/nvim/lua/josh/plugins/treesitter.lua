41e08a7a214f5190ee9ffa12ee4147e7ffdd0022
Josh Koz <josh@joshkoz.com>
2025-01-12 23:56:51 +1100

    neovim: rearrange init.lua plugins

--- neovim/.config/nvim/lua/josh/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/josh/plugins/treesitter.lua
@@ -1,56 +1,64 @@
 return {
-  -- Highlight, edit, and navigate code
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+  {
+    -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
 
-    -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-    treesitter.setup({
-      modules = {},
-      sync_install = false,
-      ignore_install = {},
-      highlight = {
-        enable = true,
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 10000
-        end, -- Disable if the buffer is too large
-        additional_vim_regex_highlighting = { "markdown" },
-      },
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "toml",
-        "html",
-        "css",
-        "c",
-        "cpp",
-        "lua",
-        "python",
-        "rust",
-        "tsx",
-        "vimdoc",
-        "vim",
-        "bash",
-        "c_sharp",
-        "markdown",
-        "markdown_inline",
-        "dockerfile",
-        "gitignore",
-      },
-      auto_install = true,
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-      },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
-    })
-  end,
+      -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+      treesitter.setup({
+        modules = {},
+        sync_install = false,
+        ignore_install = {},
+        highlight = {
+          enable = true,
+          disable = function(_, bufnr)
+            return vim.api.nvim_buf_line_count(bufnr) > 10000
+          end, -- Disable if the buffer is too large
+          additional_vim_regex_highlighting = { "markdown" },
+        },
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "toml",
+          "html",
+          "css",
+          "c",
+          "cpp",
+          "lua",
+          "python",
+          "rust",
+          "tsx",
+          "vimdoc",
+          "vim",
+          "bash",
+          "c_sharp",
+          "markdown",
+          "markdown_inline",
+          "dockerfile",
+          "gitignore",
+        },
+        auto_install = true,
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+        },
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      })
+    end,
+  },
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  },
 }
