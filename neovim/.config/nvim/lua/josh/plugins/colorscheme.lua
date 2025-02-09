4938b1977276fe2196b2a8adde44b4c9fc30c9e2
Josh Koz <josh@joshkoz.com>
2025-02-09 19:45:44 +1100

    neovim: clean up colorscheme

--- neovim/.config/nvim/lua/josh/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/josh/plugins/colorscheme.lua
@@ -19,18 +19,4 @@ return {
       vim.cmd.colorscheme("kanagawa")
     end,
   },
-  -- {
-  --   "rose-pine/neovim",
-  --   priority = 1000,
-  --   name = "rose-pine",
-  --   config = function()
-  --     require("rose-pine").setup({
-  --       styles = {
-  --         italic = false,
-  --         transparency = true,
-  --       },
-  --     })
-  --     vim.cmd("colorscheme rose-pine")
-  --   end,
-  -- },
 }
