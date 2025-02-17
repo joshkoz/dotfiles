85c69665207966d06b263a4f905ccfeaee29e6cb
Josh Koz <josh@joshkoz.com>
2025-02-17 21:19:56 +1100

    neovim: add keymaps for global marks

--- neovim/.config/nvim/lua/josh/core/options.lua
+++ neovim/.config/nvim/lua/josh/core/options.lua
@@ -92,3 +92,18 @@ vim.g.netrw_banner = 0
 
 vim.opt.inccommand = "nosplit"
 vim.opt.winheight = 5
+
+-- Per project shada file
+-- vim.opt.shadafile = (function()
+--   local data = vim.fn.stdpath("data")
+--
+--   local cwd = vim.fn.getcwd()
+--   cwd = vim.fs.root(cwd, ".git") or cwd
+--
+--   local cwd_b64 = vim.base64.encode(cwd)
+--
+--   local file = vim.fs.joinpath(data, "project_shada", cwd_b64)
+--   vim.fn.mkdir(vim.fs.dirname(file), "p")
+--
+--   return file
+-- end)()
