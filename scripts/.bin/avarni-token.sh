8daeb628150147634ad54a9e00ed1abadfc39dc3
Josh Koz <josh@joshkoz.com>
2025-03-22 00:26:48 +1100

    feat: update avarni-token script

--- scripts/.bin/avarni-token.sh
+++ scripts/.bin/avarni-token.sh
@@ -3,24 +3,21 @@
 set -e
 
 # Your 1Password item name and field designations
-ONEPASSWORD_ITEM_NAME="Avarni Platform Dev"
-AUTH0_CLIENT_SECRET=$(op item get "$ONEPASSWORD_ITEM_NAME" --fields client_secret --cache)
+ONEPASSWORD_ITEM_NAME="Avarni Platform (Dev)"
+AUTH0_CLIENT_SECRET=$(op item get "$ONEPASSWORD_ITEM_NAME" --reveal --fields client_secret)
 if [[ -z "$AUTH0_CLIENT_SECRET" ]]; then
     echo "Failed to retrieve client_secret from 1Password"
     exit 1
 fi
 
-CREDENTIALS=$(op item get "$ONEPASSWORD_ITEM_NAME" --fields client_secret,audience,domain,client_id)
+CREDENTIALS=$(op item get "$ONEPASSWORD_ITEM_NAME" --reveal --fields client_secret,audience,domain,client_id)
 
 # Split credentials into username and password
 IFS=',' read -r AUTH0_CLIENT_SECRET AUTH0_AUDIENCE AUTH0_DOMAIN AUTH0_CLIENT_ID <<< "$CREDENTIALS"
 
 # Perform the login to Auth0
 REQUEST_BODY='{"client_id":"'"$AUTH0_CLIENT_ID"'","client_secret":"'"$AUTH0_CLIENT_SECRET"'","audience":"'"$AUTH0_AUDIENCE"'","grant_type":"client_credentials"}'
-TOKEN_RESPONSE=$(curl -s --request POST \
-  --url "https://${AUTH0_DOMAIN}/oauth/token" \
-  --header 'content-type: application/json' \
-  --data $REQUEST_BODY)
+TOKEN_RESPONSE=$(curl -s --request POST --url "https://${AUTH0_DOMAIN}/oauth/token" --header 'content-type: application/json' --data $REQUEST_BODY)
 
 # Extract token from response
 ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r '.access_token')
