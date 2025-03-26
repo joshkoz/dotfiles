5a796bff91b46106b3025e9c1c8d08a72c93da66
Josh Koz <josh@joshkoz.com>
2025-03-26 20:20:46 +1100

    neovim: update blink to version 1

--- neovim/.config/nvim/lua/josh/plugins/roslyn.lua
+++ neovim/.config/nvim/lua/josh/plugins/roslyn.lua
@@ -21,6 +21,9 @@ return {
           dotnet_enable_references_code_lens = true,
           dotnet_enable_tests_code_lens = true,
         },
+        ["csharp|formatting"] = {
+          dotnet_organize_imports_on_format = true,
+        },
         ["csharp|inlay_hints"] = {
           csharp_enable_inlay_hints_for_implicit_object_creation = true,
           csharp_enable_inlay_hints_for_implicit_variable_types = true,
