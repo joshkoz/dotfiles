5ea46262262116f3267c3bfa11fa221c206c938d
Josh Koz <josh@joshkoz.com>
2026-06-13 14:22:35 +1000

    nvim: add custom file picker

--- neovim/.config/nvim/lua/josh/plugins/picker.lua
+++ neovim/.config/nvim/lua/josh/plugins/picker.lua
@@ -1,5 +1,33 @@
 local pick = require("mini.pick")
 
+local function format_path_item(item)
+  local path
+
+  if type(item) == "string" then
+    path = item
+  elseif type(item) == "table" and type(item.path) == "string" then
+    path = item.path
+  end
+
+  if path == nil then
+    return item
+  end
+
+  local name = vim.fn.fnamemodify(path, ":t")
+  local display = string.format("%s  %s", name, path)
+
+  if type(item) == "table" then
+    return vim.tbl_deep_extend("force", item, { text = display })
+  end
+
+  return { text = display, path = path }
+end
+
+local function show_filename_first(buf_id, items, query, opts)
+  local formatted = vim.tbl_map(format_path_item, items)
+  return pick.default_show(buf_id, formatted, query, opts)
+end
+
 pick.setup({
   options = {
     use_cache = true,
@@ -34,8 +62,10 @@ pick.setup({
 })
 
 vim.keymap.set("n", "<c-p>", function()
-  pick.builtin.files({ tool = "git" })
+  pick.builtin.files({ tool = "git" }, { source = { show = show_filename_first } })
 end, { desc = "Find Git Files" })
-vim.keymap.set("n", "<leader>f", pick.builtin.files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>f", function()
+  pick.builtin.files({}, { source = { show = show_filename_first } })
+end, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>h", pick.builtin.help, { desc = "Find Help" })
 vim.keymap.set("n", "<leader>/", pick.builtin.grep_live, { desc = "Find Live Grep" })
